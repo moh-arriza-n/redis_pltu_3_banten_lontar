3078a6d3bee53e6b6f202248023b214530d8e686
moh-arriza-n <arriza@linuxmail.org>
2020-02-12 10:18:58 +0700

    file semua unit get data dari redis SERVER PRO DCS

---
+++ all-unit-redis-get-MW.js
@@ -0,0 +1,6 @@
+var varunit1mw, varunit2mw = require('node-schedule');
+var varunit2mw = require('node-schedule');
+var varunit3mw = require('node-schedule');
+var varunit1mw_run = varunit1mw.scheduleJob('1-59 * * * * *',function(){
+console.log('Dayanya Sekian ...........');
+});
